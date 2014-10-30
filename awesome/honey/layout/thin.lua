c15639c09bbafe9d898b8a82c8762e99d06d8f2e
Simon Molzer <evilhowser@gmail.com>
2014-10-30 23:07:35 +0100

    Created proper module from honey

--- awesome/honey/layout/thin.lua
+++ awesome/honey/layout/thin.lua
@@ -1,23 +1,22 @@
 local math = math
-local ipair = ipair
+local ipairs = ipairs
 
 local awful = require("awful")
 local beautiful = require("beautiful")
 
 beautiful.init("~/.config/awesome/theme/theme.lua")
 
+module("honey.layout.thin")
+
 local gap = beautiful.honey.gap
-
-honey = {}
-
-honey.layout = {}
-
 -- Layout that organizes windows in a "thin" fashion
 -- Centers windows with a maximum width and organizes them in 
 -- columns, if there are more windows than columns they
 -- will be tiled vertically
-honey.layout.thin = { name = "honey_thin" }
-function honey.layout.thin.arrange(p) 
+
+name = "honey_thin"
+
+function arrange(p) 
 	local area = p.workarea
 	local clients = p.clients
 	local max_cols = beautiful.honey.thin_nr_columns
@@ -65,52 +64,3 @@ function honey.layout.thin.arrange(p)
 	end
 
 end
-
--- Layout that uses on big window in the middle of the screen 
--- Puts other clients to the left and right of the middle window
-honey.layout.focus = { name = "honey_focus" }
-function honey.layout.focus.arrange(p)
-	local area = p.workarea
-	local clients = p.clients 
-
-	local side_width = area.width / 4 - gap
-	local side_height = (area.height - gap) / 2 - gap
-
-	local positions = {
-		{
-			x = area.x + gap,
-			y = area.y + gap
-		},
-		 {
-			x = area.x + (side_width + gap) * 3,
-			y = area.y + gap
-		},
-		 {
-			x = area.x + gap,
-			y = area.y + gap * 2 + side_height
-		},
-		 {
-			x = area.x + (side_width + gap) * 3,
-			y = area.y + gap * 2 + side_height
-		},
-	}
-	for i, c in ipairs(p.clients) do
-		local g = {}
-		if i == 1 then
-			g.x = area.x + (area.width / 4) + gap
-			g.y = area.y + gap
-			g.width = (area.width / 4) * 2 - gap * 2
-			g.height = area.height - gap * 2
-		elseif i <= 5 then
-			g.x = positions[i-1].x
-			g.y = positions[i-1].y
-			g.width = side_width
-			g.height = side_height
-		else 
-			awful.client.floating.set(c, true)
-		end
-		c:geometry(g)
-	end
-end
-
-return honey
